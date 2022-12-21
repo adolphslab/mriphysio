9d0487c9f7508220d97ffa1dfdf234f6898232f4
Mike Tyszka <jmt@caltech.edu>
2022-12-21 10:58:57 -0800

    v2022.12.21 Improved robustness of cardiac fundamental frequency detection in spectrogram. Added power frequency spread metric (wSD) output in heartrate TSV for quality control.

--- mriphysio/ecg.py
+++ mriphysio/ecg.py
@@ -4,8 +4,10 @@ Class for opening and converting a DICOM physiology waveform object (CMRR) to TS
 
 import numpy as np
 import pandas as pd
-from scipy.signal import spectrogram
-import matplotlib.pyplot as plt
+from scipy.signal import spectrogram, find_peaks
+from scipy.interpolate import interp1d
+
+# import matplotlib.pyplot as plt
 
 
 class HeartRate:
@@ -65,13 +67,17 @@ class HeartRate:
             nfft=8000
         )
 
+        # Spectrogram temporal and frequency resolutions
+        dt_spec = t_spec[1] - t_spec[0]
+        df_spec = f_spec[1] - f_spec[0]
+
         print(f'Sampling frequency : {self.fs} Hz')
         print(f'Temporal samples   : {len(ecg1)}')
         print(f'Sampling time      : {np.max(t_spec)} s')
         print(f'Temporal bins      : {len(t_spec)}')
-        print(f'Temporal res       : {t_spec[1] - t_spec[0]} s')
+        print(f'Temporal res       : {dt_spec} s')
         print(f'Frequency bins     : {len(f_spec)}')
-        print(f'Frequency res      : {f_spec[1] - f_spec[0]} Hz')
+        print(f'Frequency res      : {df_spec} Hz')
 
         # Crop spectrogram frequencies to capture physiological HR range (0.5 to 3.5 Hz) and
         # RF pulse interference (approx 12.5 Hz)
@@ -80,52 +86,64 @@ class HeartRate:
         f_crop = f_spec[mask]
         sxx_crop = sxx[mask, :]
 
-        # Binarize spectrogram (10% max)
-        p_thresh = np.max(sxx_crop) / 10.0
-        sxx_bin = (sxx_crop > p_thresh).astype(float)
+        # Temporal median spectral power
+        # Robust identification of cardiac fundamental frequency over time
+        p_tmed = np.median(sxx_crop, axis=1)
 
-        # Project binarized spectrogram in time
-        p_tmax = np.max(sxx_bin, axis=1)
+        # Find peak in temporal median power closest to 1 Hz
+        i_peaks = find_peaks(p_tmed, distance=1.0 / df_spec)
+        f_peaks = f_spec[i_peaks[0]]
+        cardiac_peak = np.argmin(np.abs(f_peaks - 1.0))
+        f0 = f_peaks[cardiac_peak]
 
-        # Plot spectrogram and temporal mean power
-        # fig, axs = plt.subplots(1, 2, width_ratios=[3, 1], figsize=(16, 8), sharey=True)
-        # axs[0].pcolormesh(ts, f_crop, sxx_bin)
-        # axs[0].set_ylabel('Frequency [Hz]')
-        # axs[0].set_xlabel('Time [sec]')
-        #
-        # axs[1].plot(p_tmax, f_crop)
-        # plt.show()
+        # Create a +/- 0.5 Hz band around heart peak
+        f0_low = f0 - 0.5
+        f0_high = f0 + 0.5
 
-        # Locate fundamental frequency band in projection
-        band_indices = np.nonzero(p_tmax)[0]
-        index_jumps = (np.diff(band_indices) > 1).astype(int)
-        band_ends = np.nonzero(index_jumps)
+        print(f'Cardiac cycle peak : {f0} Hz')
+        print(f'Cardiac cycle band : {f0_low} to {f0_high} Hz')
 
-        f0_start = band_indices[0]
-        f0_end = band_indices[band_ends[0][0]] + 1
-
-        print(f'Fundamental band   : [{f_spec[f0_start]:0.3f}, {f_spec[f0_end]:0.3f}) Hz')
+        # Find indices closest to heart band limits
+        interpolator = interp1d(f_spec, np.arange(len(f_spec)), kind='nearest')
+        i_low, i_high = interpolator([f0_low, f0_high]).astype(int)
 
         # Calculate weighted mean fundamental frequency from spectrogram columns
 
         # Carve out fundamental band in spectrogram
-        Sxx_f0 = sxx_crop[f0_start:f0_end, :]
-        f_f0 = f_crop[f0_start:f0_end]
+        sxx_f0 = sxx_crop[i_low:i_high, :]
+        f_f0 = f_crop[i_low:i_high]
 
         # Fundamental frequency from power-weighted mean frequency
-        hr = np.matmul(Sxx_f0.transpose(), f_f0) / np.sum(Sxx_f0, axis=0)
+        f0 = np.matmul(sxx_f0.transpose(), f_f0) / np.sum(sxx_f0, axis=0)
 
         # fig, axs = plt.subplots(1, 1, figsize=(16, 8))
-        # axs.pcolormesh(t_spec, f_crop, sxx_crop)
+        # axs.pcolormesh(t_spec, f_crop, np.log(sxx_crop))
         # axs.set_ylabel('Frequency [Hz]')
         # axs.set_xlabel('Time [sec]')
-        # axs.plot(t_spec, hr, 'w')
-        # axs.set_ylim(0, 3.5)
+        # axs.plot(t_spec, f0, 'white', linewidth=2)
+        # axs.set_ylim(0, 5)
         # plt.show()
 
+        # ECG waveform quality control
+        # Calculate weighted sd of frequency in cardiac fundamental band.
+        print('\nRunning quality control on ECG waveform')
+
+        # Sum over frequency of fundamental band spectrogram
+        # Use for weight normalization
+        sxx_f0_fsum = np.sum(sxx_f0, axis=0)
+
+        # Power-weighted SD over frequency
+        wsd = np.zeros_like(t_spec)
+        for tc, tt in enumerate(t_spec):
+            wsd[tc] = np.sqrt(np.sum(sxx_f0[:, tc] * (f_f0 - f0[tc]) ** 2) / sxx_f0_fsum[tc])
+
+        # Weighted SDs > 0.15 Hz generally indicate poor ECG quality in that time bin
+        qc_pass = wsd < 0.15
+        print(f'Quality control pass rate : {np.sum(qc_pass) / len(qc_pass) * 100.0:0.1f} %')
+
         # Construct heart rate dataframe
-        hr_array = np.vstack([t_spec, hr]).transpose()
-        self.hr = pd.DataFrame(hr_array, columns=['Time (s)', 'Heart Rate (Hz)'])
+        hr_array = np.vstack([t_spec, f0, wsd]).transpose()
+        self.hr = pd.DataFrame(hr_array, columns=['Time (s)', 'Heart Rate (Hz)', 'wSD (Hz)'])
 
         return self.hr
 
